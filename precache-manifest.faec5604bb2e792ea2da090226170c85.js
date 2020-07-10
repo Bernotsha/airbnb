a9a83d8e2b2ff1eabedeed8ac17b8a8483ba9da3
BERNOTSHA <bernotshasekar277@gmail.com>
2020-07-10 10:52:39 +0530

    Updates

---
+++ precache-manifest.faec5604bb2e792ea2da090226170c85.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f1477578a3bc9408865ab164531739",
+    "url": "/airbnbapp/index.html"
+  },
+  {
+    "revision": "19d4b5a925ebce9f91c6",
+    "url": "/airbnbapp/static/css/main.2814cb1b.chunk.css"
+  },
+  {
+    "revision": "496c9468d0379d955804",
+    "url": "/airbnbapp/static/js/2.80c8bf22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/airbnbapp/static/js/2.80c8bf22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19d4b5a925ebce9f91c6",
+    "url": "/airbnbapp/static/js/main.80c14f7d.chunk.js"
+  },
+  {
+    "revision": "91ef48256867788787b7",
+    "url": "/airbnbapp/static/js/runtime-main.abea4b61.js"
+  },
+  {
+    "revision": "b5752b17e8f4d83214f3b7947d5e0a84",
+    "url": "/airbnbapp/static/media/bmb2.b5752b17.png"
+  },
+  {
+    "revision": "49bfa2ecafe5f663278424e144c7cef6",
+    "url": "/airbnbapp/static/media/bnb3.49bfa2ec.jpg"
+  },
+  {
+    "revision": "84a40241c9f43c47db3800ca9614e505",
+    "url": "/airbnbapp/static/media/bnbicon.84a40241.png"
+  },
+  {
+    "revision": "1521a5cc33862513d7f9b981ca3dc385",
+    "url": "/airbnbapp/static/media/bng1.1521a5cc.jpeg"
+  },
+  {
+    "revision": "880a558d8f3f174af342ef1df25ebd85",
+    "url": "/airbnbapp/static/media/chef2.880a558d.png"
+  },
+  {
+    "revision": "d037a6230515c2ed821b8d2c0aa358dc",
+    "url": "/airbnbapp/static/media/chef4.d037a623.jpg"
+  },
+  {
+    "revision": "b7252afe1c60d5bba89f2be805567f76",
+    "url": "/airbnbapp/static/media/facebookicon.b7252afe.png"
+  },
+  {
+    "revision": "20cfcc11c7771579be91c45a1d537f16",
+    "url": "/airbnbapp/static/media/food.20cfcc11.jpg"
+  }
+]);
